dfab506ae78a2c0a61e5357a593e93b10bf0ac97
mansurissa <mansurissa6@gmail.com>
2020-08-28 21:35:45 +0200

    color change and testinomials, and admin page design but not functional

---
+++ admin/firebase.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB8Ms2Z36_Omo3p7tfysmGZ8MukFPqs1T0",
+  authDomain: "afrinet-f6988.firebaseapp.com",
+  databaseURL: "https://afrinet-f6988.firebaseio.com",
+  projectId: "afrinet-f6988",
+  storageBucket: "afrinet-f6988.appspot.com",
+  messagingSenderId: "362732637545",
+  appId: "1:362732637545:web:a457dd419813a6f4b881f8",
+  measurementId: "G-00L79WCX5T",
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+const loginForm = document.querySelector("#login-form");
+const auth = firebase.auth();
